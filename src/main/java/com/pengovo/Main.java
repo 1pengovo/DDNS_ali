a158895abe6d18dc8a3b3933bc4b3496c6a79134
pengovo <1501218790@qq.com>
2023-09-19 14:07:02 +0800

    update 实现多域名配置更新

--- src/main/java/com/pengovo/Main.java
+++ src/main/java/com/pengovo/Main.java
@@ -19,88 +19,117 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     private static Integer interval = null;
+    private static String currentHostIP = null;
 
     public static void main(String[] args) throws Exception {
-
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-interval")) {
-                if (i + 1 < args.length) {
-                    interval = Integer.valueOf(args[i + 1]);
-                    if (interval <= 0) {
-                        com.aliyun.teaconsole.Client.log("-interval参数不合法，应输入大于0的整数");
-                        System.exit(1);
-                    }
-                    i++; // 跳过下一个参数，因为它是文件名
-                } else {
-                    com.aliyun.teaconsole.Client.log("-interval后缺失参数");
-                    System.exit(1);
-                }
-            }
-        }
+        parseArguments(args);
 
         if (interval != null) {
-            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
-            // 初始延迟为0秒，然后每隔多少分钟执行一次
-            scheduler.scheduleAtFixedRate(() -> {
-                try {
-                    update();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }, 0, interval, TimeUnit.MINUTES);
+            scheduleUpdateTask();
         } else {
             update();
         }
     }
 
-    public static void update() throws Exception {
-        LocalDateTime now = LocalDateTime.now();
-        com.aliyun.teaconsole.Client.log("-------------------开始执行更新操作，当前时间为：" + now + "--------------------");
-        String regionId = (String) ConfigUtils.getClientConfigs().get("regionId");
+    // 解析命令行参数
+    private static void parseArguments(String[] args) throws Exception {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-interval")) {
+                if (i + 1 < args.length) {
+                    interval = Integer.valueOf(args[i + 1]);
+                    if (interval <= 0) {
+                        logAndExit("-interval参数不合法，应输入大于0的整数");
+                    }
+                    i++; // 跳过下一个参数，因为它是文件名
+                } else {
+                    logAndExit("-interval后缺失参数");
+                }
+            }
+        }
+    }
 
+    // 定期执行更新任务
+    private static void scheduleUpdateTask() {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        // 初始延迟为0秒，然后每隔多少分钟执行一次
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                update();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }, 0, interval, TimeUnit.MINUTES);
+    }
+
+    // 执行更新操作
+    private static void update() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        log("-------------------开始执行更新操作，当前时间为：" + now + "--------------------");
+        currentHostIP = IPUtils.getOutIPV4();
+        log("-------------------当前公网ip为：" + currentHostIP + "--------------------");
+        String regionId = (String) ConfigUtils.getClientConfigs().get("regionId");
         List<Map<String, Object>> domainConfigs = ConfigUtils.getDomainConfigs();
 
         if (domainConfigs != null) {
             for (Map<String, Object> domainConfig : domainConfigs) {
-                String domainName = (String) domainConfig.get("domainName");
-                String RR = (String) domainConfig.get("RR");
-                String recordType = (String) domainConfig.get("recordType");
-                String currentHostIP = IPUtils.getOutIPV4();
-
-                System.out.println("Domain Name: " + domainName);
-                System.out.println("RR: " + RR);
-                System.out.println("Record Type: " + recordType);
-                System.out.println();
-                com.aliyun.alidns20150109.Client client = DnsUtils.Initialization(regionId);
-                DescribeDomainRecordsResponse resp = DnsUtils.DescribeDomainRecords(client, domainName, RR, recordType);
-                if (com.aliyun.teautil.Common.isUnset(TeaModel.buildMap(resp)) || com.aliyun.teautil.Common.isUnset(TeaModel.buildMap(resp.body.domainRecords.record.get(0)))) {
-                    com.aliyun.teaconsole.Client.log("错误参数！");
-                    return;
-                }
-
-                DescribeDomainRecordsResponseBody.DescribeDomainRecordsResponseBodyDomainRecordsRecord record = resp.body.domainRecords.record.get(0);
-                // 记录ID
-                String recordId = record.recordId;
-                // 记录值
-                String recordsValue = record.value;
-                com.aliyun.teaconsole.Client.log("-------------------当前主机公网IP为：" + currentHostIP + "--------------------");
-                if (!com.aliyun.teautil.Common.equalString(currentHostIP, recordsValue)) {
-                    // 修改解析记录
-                    UpdateDomainRecordRequest req = new UpdateDomainRecordRequest();
-                    // 主机记录
-                    req.RR = RR;
-                    // 记录ID
-                    req.recordId = recordId;
-                    // 将主机记录值改为当前主机IP
-                    req.value = currentHostIP;
-                    // 解析记录类型
-                    req.type = recordType;
-                    DnsUtils.UpdateDomainRecord(client, req);
-                } else {
-                    com.aliyun.teaconsole.Client.log("当前解析记录已是最新的了");
-                }
+                handleDomainUpdate(regionId, domainConfig);
             }
         }
+    }
+
+    // 处理单个域名更新
+    private static void handleDomainUpdate(String regionId, Map<String, Object> domainConfig) throws Exception {
+        String domainName = (String) domainConfig.get("domainName");
+        String RR = (String) domainConfig.get("RR");
+        String recordType = (String) domainConfig.get("recordType");
+
+        log("-------------------正在更新以下配置--------------------");
+        log("Domain Name: " + domainName);
+        log("RR: " + RR);
+        log("Record Type: " + recordType);
+        log("-----------------------------------------------");
+        com.aliyun.alidns20150109.Client client = DnsUtils.Initialization(regionId);
+        DescribeDomainRecordsResponse resp = DnsUtils.DescribeDomainRecords(client, domainName, RR, recordType);
+        if (com.aliyun.teautil.Common.isUnset(TeaModel.buildMap(resp)) || com.aliyun.teautil.Common.isUnset(TeaModel.buildMap(resp.body.domainRecords.record.get(0)))) {
+            log("错误参数！");
+            return;
+        }
+        DescribeDomainRecordsResponseBody.DescribeDomainRecordsResponseBodyDomainRecordsRecord record = resp.body.domainRecords.record.get(0);
+        // 记录ID
+        String recordId = record.recordId;
+        // 记录值
+        String recordsValue = record.value;
+        if (!currentHostIP.equals(recordsValue)) {
+            // 修改解析记录
+            UpdateDomainRecordRequest req = buildUpdateRequest(RR, recordId, currentHostIP, recordType);
+            DnsUtils.UpdateDomainRecord(client, req);
+        } else {
+            log("当前解析记录已是最新的了");
+        }
+    }
+
+    private static UpdateDomainRecordRequest buildUpdateRequest(String RR, String recordId, String currentHostIP, String recordType) {
+        UpdateDomainRecordRequest req = new UpdateDomainRecordRequest();
+        // 主机记录
+        req.RR = RR;
+        // 记录ID
+        req.recordId = recordId;
+        // 将主机记录值改为当前主机IP
+        req.value = currentHostIP;
+        // 解析记录类型
+        req.type = recordType;
+        return req;
+    }
+
+    // 日志输出
+    private static void log(String message) throws Exception {
+        com.aliyun.teaconsole.Client.log(message);
+    }
+
+    // 日志输出并退出程序
+    private static void logAndExit(String message) throws Exception {
+        log(message);
+        System.exit(1);
     }
 }
